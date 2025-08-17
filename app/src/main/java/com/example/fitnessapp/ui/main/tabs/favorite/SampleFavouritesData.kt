81e6e2c337ef1a6d1c11d65f7a4f153365749a1b
valdo0 <sebastian.valdivia97gmail.com>
2025-08-16 20:40:15 -0400

    Añade datos de recetas favoritas de muestra en SampleFavouritesData.kt

---
+++ app/src/main/java/com/example/fitnessapp/ui/main/tabs/favorite/SampleFavouritesData.kt
@@ -0,0 +1,72 @@
+package com.example.fitnessapp.ui.main.tabs.favorite
+
+
+import com.example.fitnessapp.R
+import com.example.fitnessapp.data.model.RecipeItem
+
+val sampleFavouritesRecipes = listOf(
+    RecipeItem(
+        id = "1",
+        title = "Ensalada quinoa con palta",
+        subtitle = "Una ensalada fresca y nutritiva.",
+        imageUrl = "https://images.cookforyourlife.org/wp-content/uploads/2022/09/Citrus-Avocado-Salad.png",
+        placeholderResId = R.drawable.placeholder_food
+    ),
+    RecipeItem(
+        id = "2",
+        title = "Salmón al Horno con hiervas y limón.",
+        subtitle = "Un almuerzo completo y lleno de sabor.",
+        imageUrl = "https://assets.tmecosys.com/image/upload/t_web_rdp_recipe_584x480/img/recipe/ras/Assets/3165C6C3-0E36-4D97-B740-B8E36A35762B/Derivates/b27829d5-8817-4989-9228-14da13505e05.jpg",
+        placeholderResId = R.drawable.placeholder_food
+    ),
+    RecipeItem(
+        id = "3",
+        title = "Batido Energético",
+        subtitle = "Frutas y proteína.",
+        imageUrl = null,
+        placeholderResId = R.drawable.placeholder_food
+    ),
+    RecipeItem(
+        id = "4",
+        title = "Pollo a la Plancha",
+        subtitle = "Marinado en especias.",
+        imageUrl = null,
+        placeholderResId = R.drawable.placeholder_food
+    ),
+    RecipeItem(
+        id = "5",
+        title = "Avena Nocturna",
+        subtitle = "Preparación rápida y nutritiva.",
+        imageUrl = null,
+        placeholderResId = R.drawable.placeholder_food
+    ),
+    RecipeItem(
+        id = "6",
+        title = "Avena Nocturna",
+        subtitle = "Preparación rápida y nutritiva.",
+        imageUrl = null,
+        placeholderResId = R.drawable.placeholder_food
+    ),
+    RecipeItem(
+        id = "7",
+        title = "Avena Nocturna",
+        subtitle = "Preparación rápida y nutritiva.",
+        imageUrl = null,
+        placeholderResId = R.drawable.placeholder_food
+    ),
+    RecipeItem(
+        id = "8",
+        title = "Avena Nocturna",
+        subtitle = "Preparación rápida y nutritiva.",
+        imageUrl = null,
+        placeholderResId = R.drawable.placeholder_food
+    ),
+    RecipeItem(
+        id = "9",
+        title = "Avena Nocturna",
+        subtitle = "Preparación rápida y nutritiva.",
+        imageUrl = null,
+        placeholderResId = R.drawable.placeholder_food
+    ),
+
+)
