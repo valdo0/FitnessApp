61ee2f1a836e7c761230bc2b2714b868c1094225
valdo0 <sebastian.valdivia97gmail.com>
2025-08-16 20:40:30 -0400

    Añade transiciones de animación a la navegación en AppNavigation

--- app/src/main/java/com/example/fitnessapp/ui/navigation/AppNavigation.kt
+++ app/src/main/java/com/example/fitnessapp/ui/navigation/AppNavigation.kt
@@ -1,11 +1,16 @@
 package com.example.fitnessapp.ui.navigation
 
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.fitnessapp.ui.auth.login.LoginScreen
+import com.example.fitnessapp.ui.auth.recoverPassword.RecoverPasswordScreen
 import com.example.fitnessapp.ui.auth.register.RegisterScreen
 import com.example.fitnessapp.ui.main.MainScreen
 
@@ -16,6 +21,18 @@ fun AppNavigation(modifier: Modifier = Modifier) {
     NavHost(
         navController = navController,
         startDestination = AppScreens.LoginScreen.route,
+        enterTransition = {
+            slideInHorizontally(initialOffsetX = { 1000 }) + fadeIn(initialAlpha = 0.1f)
+        },
+        exitTransition = {
+            slideOutHorizontally(targetOffsetX = { -1000 }) + fadeOut(targetAlpha = 0.1f)
+        },
+        popEnterTransition = {
+            slideInHorizontally(initialOffsetX = { -1000 }) + fadeIn(initialAlpha = 0.1f)
+        },
+        popExitTransition = {
+            slideOutHorizontally(targetOffsetX = { 1000 }) + fadeOut(targetAlpha = 0.1f)
+        }
 
     ) {
         composable(route = AppScreens.LoginScreen.route) {
@@ -23,6 +40,10 @@ fun AppNavigation(modifier: Modifier = Modifier) {
         }
         composable(route = AppScreens.RegisterScreen.route) {
             RegisterScreen(navController = navController)
+        }
+
+        composable(route = AppScreens.RecoverPasswordScreen.route) {
+            RecoverPasswordScreen(navController = navController)
         }
         composable(route = AppScreens.MainScreen.route) {
             MainScreen(  mainNavController = navController )
