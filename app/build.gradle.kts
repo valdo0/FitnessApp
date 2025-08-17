5ca5cc03bf820bfdf7c7c1d08b05c3bbaa9c5eae
valdo0 <sebastian.valdivia97gmail.com>
2025-08-16 20:39:23 -0400

    Añade la dependencia de Accompanist para la animación de navegación.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,7 @@ dependencies {
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.core.splashscreen)
     implementation("io.coil-kt:coil-compose:2.6.0")
+    implementation("com.google.accompanist:accompanist-navigation-animation:0.32.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
